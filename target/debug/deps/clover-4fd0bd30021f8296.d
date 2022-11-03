ba4c5abe84f4ee4bfd5951a1719e9b018b2a6b48
elfalehed <mohamedelfaleh5@gmail.com>
2022-11-03 15:47:55 +0100

    README for next plans

--- target/debug/deps/clover-4fd0bd30021f8296.d
+++ target/debug/deps/clover-4fd0bd30021f8296.d
@@ -1,6 +1,6 @@
-/home/mohamed/Desktop/clover/target/debug/deps/clover-4fd0bd30021f8296.rmeta: src/main.rs src/lexer/mod.rs src/lexer/token.rs
+/home/mohamed/Desktop/clover-main/target/debug/deps/clover-4fd0bd30021f8296.rmeta: src/main.rs src/lexer/mod.rs src/lexer/token.rs
 
-/home/mohamed/Desktop/clover/target/debug/deps/clover-4fd0bd30021f8296.d: src/main.rs src/lexer/mod.rs src/lexer/token.rs
+/home/mohamed/Desktop/clover-main/target/debug/deps/clover-4fd0bd30021f8296.d: src/main.rs src/lexer/mod.rs src/lexer/token.rs
 
 src/main.rs:
 src/lexer/mod.rs:
--- target/debug/deps/clover-ee551edb99701197.d
+++ target/debug/deps/clover-ee551edb99701197.d
@@ -1,6 +1,6 @@
-/home/mohamed/Desktop/clover/target/debug/deps/clover-ee551edb99701197.rmeta: src/main.rs src/lexer/mod.rs src/lexer/token.rs
+/home/mohamed/Desktop/clover-main/target/debug/deps/clover-ee551edb99701197.rmeta: src/main.rs src/lexer/mod.rs src/lexer/token.rs
 
-/home/mohamed/Desktop/clover/target/debug/deps/clover-ee551edb99701197.d: src/main.rs src/lexer/mod.rs src/lexer/token.rs
+/home/mohamed/Desktop/clover-main/target/debug/deps/clover-ee551edb99701197.d: src/main.rs src/lexer/mod.rs src/lexer/token.rs
 
 src/main.rs:
 src/lexer/mod.rs:
